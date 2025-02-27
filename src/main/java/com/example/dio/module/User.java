bb0854c824b393240312a3c9cb2727248bc77258
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-02-27 14:23:45 +0530

    Modified User Entity

--- src/main/java/com/example/dio/module/User.java
+++ src/main/java/com/example/dio/module/User.java
@@ -1,22 +1,35 @@
 package com.example.dio.module;
 
 
+import com.example.dio.UserRole;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name="user")
 @Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private long userid;
+    @Column(name = "user_name")
     private String username;
+    @Column(name = "email")
     private String useremail;
+    @Column(name = "password")
     private  String userpassword;
-    private  int userphoneNo;
-    private  String userrole;
+    @Column(name = "pho_no")
+    private  String  userphoneNo;
+    @Column(name = "role")
+    private UserRole userrole;
+    @Column(name = "created_at")
     private LocalDate createdAt;
+    @Column(name = "last_modified_at")
     private  LocalDate lastModifiedAt;
 }
