518fa1496d315e1727cf128f940bd29fdda23a41
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-02-27 14:24:30 +0530

    Modified staff entity

--- src/main/java/com/example/dio/module/Staff.java
+++ src/main/java/com/example/dio/module/Staff.java
@@ -1,5 +1,13 @@
 package com.example.dio.module;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Entity
 public class Staff extends User{
 
 }
