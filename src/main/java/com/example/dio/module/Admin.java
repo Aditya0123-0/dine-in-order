90ad8bf10975b2ddd36984c296ef2324efd9e06a
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-02-27 14:25:18 +0530

    Modified Admin entity

--- src/main/java/com/example/dio/module/Admin.java
+++ src/main/java/com/example/dio/module/Admin.java
@@ -1,5 +1,11 @@
 package com.example.dio.module;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Admin extends User{
 
 }
