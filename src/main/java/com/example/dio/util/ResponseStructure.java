d5b3cc6d8a5fc86d3b4156d9b59589dc3f1adfaa
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-02-27 16:48:34 +0530

    Added Response Structure

--- src/main/java/com/example/dio/util/ResponseStructure.java
+++ src/main/java/com/example/dio/util/ResponseStructure.java
@@ -1,13 +1,24 @@
 package com.example.dio.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.example.dio.module.User;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseStructure<T> {
     private int status;
     private String message;
     private T data;
+
+//    Created factor method for ResponseStructure<T> by creating Generic factory method
+    public static <T> ResponseStructure<T> create(HttpStatus status,String message,T data){
+        ResponseStructure<T> response = new ResponseStructure<>();
+        response.status= status.value();
+        response.message=message;
+        response.data = data;
+        return  response;
+    }
 }
