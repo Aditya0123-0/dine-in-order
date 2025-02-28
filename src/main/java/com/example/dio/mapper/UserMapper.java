de198cfbd06489b1f9760ecc03b4154f527674f9
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-02-28 19:03:16 +0530

    Created mapper

--- src/main/java/com/example/dio/mapper/UserMapper.java
+++ src/main/java/com/example/dio/mapper/UserMapper.java
@@ -4,32 +4,15 @@ import com.example.dio.dto.request.RegistrationRequest;
 import com.example.dio.dto.request.UserRequest;
 import com.example.dio.dto.response.UserResponse;
 import com.example.dio.module.User;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
-@Component
-public class UserMapper {
-    public UserResponse mapToUserResponse(User user) {
-        return UserResponse.builder()
-                .userId(user.getUserid())
-                .username(user.getUsername())
-                .userrole(user.getUserrole())
-                .createdAt(user.getCreatedAt())
-                .lastModifiedAt(user.getLastModifiedAt())
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+     UserResponse mapToUserResponse(User user) ;
 
 
-    public void mapUserToEntity(RegistrationRequest registrationRequest, User user) {
-        user.setUsername(registrationRequest.getUsername());
-        user.setUseremail(registrationRequest.getUseremail());
-        user.setUserpassword(registrationRequest.getUserpassword());
-        user.setUserphoneNo(registrationRequest.getUserphoneNo());
-        user.setUserrole(registrationRequest.getUserrole());
-    }
+     void mapUserToEntity(RegistrationRequest registrationRequest,@MappingTarget User user) ;
 
-    public void mapUserToEntity(UserRequest userRequest, User user) {
-        user.setUsername(userRequest.getUsername());
-        user.setUseremail(userRequest.getUseremail());
-        user.setUserphoneNo(userRequest.getUserphoneNo());
-    }
+     void mapUserToEntity(UserRequest userRequest,@MappingTarget User user) ;
 }
