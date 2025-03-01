46d946550cbcb20a34c3fc83fd2ffa2f702493be
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-03-01 14:58:05 +0530

    Modified pom.xml

--- src/main/java/com/example/dio/dto/response/UserResponse.java
+++ src/main/java/com/example/dio/dto/response/UserResponse.java
@@ -1,9 +1,7 @@
 package com.example.dio.dto.response;
 
 import com.example.dio.UserRole;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
