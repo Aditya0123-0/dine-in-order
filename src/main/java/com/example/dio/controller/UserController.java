0a48fd64c7ef123a7cf7932ba901cf0840c0183d
Aditya0123-0 <adityawadkarofficial@gmail.com>
2025-03-04 16:27:35 +0530

    Added swagger for documentation

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -4,8 +4,15 @@ import com.example.dio.Service.UserService;
 import com.example.dio.dto.request.RegistrationRequest;
 import com.example.dio.dto.request.UserRequest;
 import com.example.dio.dto.response.UserResponse;
+import com.example.dio.util.FieldErrorResponse;
 import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
+import com.example.dio.util.SimpleErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,24 +21,59 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/register")
+@RequestMapping("/api/v1")
+@Tag(name = "User Controller", description = "Collection API endpoints dealing with user data")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/register")
+    @Operation(description = """
+            The API endpoint is used to register user.
+            The endpoint requires the user to select one of the Specific role along with the other details.
+            """ , responses = {
+            @ApiResponse(responseCode = "201", description = "User Created"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = {
+                    @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+            })
+    })
     public ResponseEntity<ResponseStructure<UserResponse>> register(@RequestBody @Valid RegistrationRequest registrationRequest){
         UserResponse response = userService.registerUser(registrationRequest);
         return ResponseBuilder.success(HttpStatus.CREATED,"Created User", response);
     }
 
+
+
+
     @GetMapping("/{userid}")
+    @Operation(description = """
+             The API endpoint is used to get the user using userid.
+             The endpoint returns the details of user except users email, password and phone number.
+             """,responses = {
+            @ApiResponse(responseCode = "201", description = "Found User"),
+            @ApiResponse(responseCode = "400", description = "Invalid userId", content = {
+                    @Content(schema = @Schema(implementation = SimpleErrorResponse.class))
+            })
+    })
     public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable Long userid){
         UserResponse response = userService.findUserById(userid);
         return  ResponseBuilder.success(HttpStatus.OK,"Found User",response);
     }
 
+
+
+
+
     @PutMapping("/{userid}")
+    @Operation(description = """
+            The API endpoint is used to update the user data depending on userid.
+            This endpoint allows user to update only Username , email, phone number and restrict the sensitive details form updating.
+            """, responses = {
+            @ApiResponse(responseCode = "201", description = "User Updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input" ,
+            content = @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+            )
+    })
     public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@PathVariable Long userid, @RequestBody UserRequest request){
         UserResponse response = userService.updateUserById(userid,request);
         return ResponseBuilder.success(HttpStatus.OK,"User Updated",response);
--- src/main/java/com/example/dio/dto/request/UserRequest.java
+++ src/main/java/com/example/dio/dto/request/UserRequest.java
@@ -1,14 +1,28 @@
 package com.example.dio.dto.request;
 
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequest {
+    @NotNull(message = "username cannot be null")
+    @NotBlank(message = "username cannot br blank")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can contain only Alphabets,numbers and underscore")
     private String username;
+
+    @NotNull(message = "email must not be null")
+    @NotBlank(message = "email cannot be blank")
+    @Email(regexp = "^([\\w]*[\\w\\.]*(?!\\.)@gmail.com)$", message ="email must be in the form xyz@gmail.com")
     private String useremail;
+
+
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Contain At least 8 character,At least one uppercase English letter," +
+            "At least one lowercase English letter, "+"At least one digit, At least one special character")
     private  String  userphoneNo;
 
 }
